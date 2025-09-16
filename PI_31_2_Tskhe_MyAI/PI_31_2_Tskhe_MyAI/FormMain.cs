133287e0983045c4d8dfb09344ed6edd79aa79cc
chuxlee <youngartist7009@gmail.com>
2025-09-16 10:47:08 +0300

    Add files via upload

---
+++ PI_31_2_Tskhe_MyAI/PI_31_2_Tskhe_MyAI/FormMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_31_2_Tskhe_MyAI
+{
+    public partial class FormMain : Form
+    {
+        private double[] inputPixels;
+        public FormMain()
+        {
+            InitializeComponent();
+
+            inputPixels = new double[15];
+        }
+
+        private void Changing_State_Pixel_Button_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).BackColor == Color.White)
+            {
+                ((Button)sender).BackColor = Color.Black;
+                inputPixels[((Button)sender).TabIndex] = 1d;
+            }
+            else
+            {
+                ((Button)sender).BackColor = Color.White;
+                inputPixels[((Button)sender).TabIndex] = 0d;
+            }
+        }
+    }
+}
